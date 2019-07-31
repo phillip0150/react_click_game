5d07fad28f74a0fea813af4f18b7da68f630e171
Phillip Valdez <Phillip0150@gmail.com>
2019-07-30 22:58:47 -0500

    Updates

---
+++ precache-manifest.2152e234c6f1b82faf29a86e3e5608ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a53ba2c55242b274c97ea50d7bcf608",
+    "url": "/react_click_game/index.html"
+  },
+  {
+    "revision": "a00cbec502ee7341c09d",
+    "url": "/react_click_game/static/css/main.61cf5b8c.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/react_click_game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "a00cbec502ee7341c09d",
+    "url": "/react_click_game/static/js/main.a1e0cf5f.chunk.js"
+  },
+  {
+    "revision": "7b83946404a49a80cabb",
+    "url": "/react_click_game/static/js/runtime~main.902fbdad.js"
+  }
+]);
